32427f570be2f19e2785cbb87ef126ea2e91b8d1
Andy <97757029+AbnormalAndy@users.noreply.github.com>
2024-02-15 21:24:31 -0600

    Refactor hello.go and hello_test.go.

--- GoFundamentals/HelloWorld/hello_test.go
+++ GoFundamentals/HelloWorld/hello_test.go
@@ -3,9 +3,20 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Meow")
-	want := "Hello, Meow."
+	t.Run("Saying hello to people.", func(t *testing.T) {
+		got := Hello("Meow")
+		want := "Hello, Meow."
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("Empty string defaults to 'world'.", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world."
+		assertCorrectMessage(t, got, want)
+	})
+}
 
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("Got %q; want %q.", got, want)
 	}
