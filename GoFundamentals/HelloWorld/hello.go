8279f4c681d9b49971553d5dc1c4e607ad4ad556
Andy <97757029+AbnormalAndy@users.noreply.github.com>
2024-03-01 10:28:36 -0600

    Refactor and improve scalability.

--- GoFundamentals/HelloWorld/hello.go
+++ GoFundamentals/HelloWorld/hello.go
@@ -2,15 +2,40 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
+const (
+	french  = "French"
+	russian = "Russian"
+	spanish = "Spanish"
 
-func Hello(name string) string {
+	englishHelloPrefix = "Hello, "
+	frenchHelloPrefix  = "Bonjour, "
+	russianHelloPrefix = "Привет, "
+	spanishHelloPrefix = "Hola, "
+)
+
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "world"
 	}
-	return englishHelloPrefix + name + "."
+
+	return greetingPrefix(language) + name + "."
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case russian:
+		prefix = russianHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
 }
 
 func main() {
-	fmt.Println(Hello(""))
+	fmt.Println(Hello("", ""))
 }
--- GoFundamentals/HelloWorld/hello_test.go
+++ GoFundamentals/HelloWorld/hello_test.go
@@ -4,15 +4,31 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("Saying hello to people.", func(t *testing.T) {
-		got := Hello("Meow")
+		got := Hello("Meow", "")
 		want := "Hello, Meow."
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("Empty string defaults to 'world'.", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, world."
 		assertCorrectMessage(t, got, want)
 	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Meow", "French")
+		want := "Bonjour, Meow."
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Russian", func(t *testing.T) {
+		got := Hello("Meow", "Russian")
+		want := "Привет, Meow."
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie."
+		assertCorrectMessage(t, got, want)
+	})
+
 }
 
 func assertCorrectMessage(t testing.TB, got, want string) {
