0fcc86fa2309bbf3ac8d03dae307d05a01437364
Andy <97757029+AbnormalAndy@users.noreply.github.com>
2024-05-05 18:24:39 -0500

    Add code to Buy function.

--- ForTheLoveOfGo/happy-fun-books/bookstore.go
+++ ForTheLoveOfGo/happy-fun-books/bookstore.go
@@ -1,13 +1,20 @@
 package bookstore
 
+import (
+	"errors"
+)
+
 type Book struct {
 	Title	string
 	Author	string
 	Copies	int
 }
 
-func Buy(b Book) Book {
+func Buy(b Book) (Book, error) {
+	if b.Copies == 0 {
+		return Book{}, errors.New("No copies left.")
+	}
 	b.Copies--
-	return b 
+	return b, nil 
 }
 
--- ForTheLoveOfGo/happy-fun-books/bookstore_test.go
+++ ForTheLoveOfGo/happy-fun-books/bookstore_test.go
@@ -22,10 +22,26 @@ func TestBuy(t *testing.T) {
 		Copies:	2,
 	}
 	want := 1
-	result := bookstore.Buy(b)
+	result, err := bookstore.Buy(b)
+	if err != nil {
+		t.Fatal(err)
+	}
 	got := result.Copies
 	if want != got {
 		t.Errorf("Want %d; got %d.", want, got)
+	}
+}
+
+func TestBuyErrorsIfNoCopiesLeft(t *testing.T) {
+	t.Parallel()
+	b := bookstore.Book{
+		Title:	"Spark Joy",
+		Author:	"Marie Kondo",
+		Copies:	0,
+	}
+	_, err := bookstore.Buy(b)
+	if err == nil {
+		t.Error("Want error buying from zero copies. Got nil.")
 	}
 }
 
