daf06fc78813f1ce614013b00cb560437075159b
Andy <97757029+AbnormalAndy@users.noreply.github.com>
2024-05-12 11:37:26 -0500

    Add SetCategory.

--- ForTheLoveOfGo/happy-fun-books/bookstore.go
+++ ForTheLoveOfGo/happy-fun-books/bookstore.go
@@ -12,6 +12,7 @@ type Book struct {
 	Copies		int
 	PriceCents	int
 	DiscountPercent	int
+	category	string
 }
 
 func Buy(b Book) (Book, error) {
@@ -52,5 +53,17 @@ func (b *Book) SetPriceCents(price int) error {
 		return fmt.Errorf("Negative price %d.", price)
 	}
 	b.PriceCents = price
+	return nil
+}
+
+func (b Book) Category() string {
+	return b.category
+}
+
+func (b *Book) SetCategory(category string) error {
+	if category != "Autobiography" {
+		return fmt.Errorf("Unknown category %q.", category)
+	}
+	b.category = category
 	return nil
 }
--- ForTheLoveOfGo/happy-fun-books/bookstore_test.go
+++ ForTheLoveOfGo/happy-fun-books/bookstore_test.go
@@ -5,6 +5,7 @@ import (
 	"sort"
 	"testing"
 	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
 func TestBook(t *testing.T) {
@@ -61,7 +62,7 @@ func TestGetAllBooks(t *testing.T) {
 	sort.Slice(got, func(i, j int) bool {
 		return got[i].ID < got[j].ID
 	})
-	if !cmp.Equal(want, got) {
+	if !cmp.Equal(want, got, cmpopts.IgnoreUnexported(bookstore.Book{})) {
 		t.Errorf(cmp.Diff(want, got))
 	}
 }
@@ -80,7 +81,7 @@ func TestGetBook(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	if !cmp.Equal(want, got) {
+	if !cmp.Equal(want, got, cmpopts.IgnoreUnexported(bookstore.Book{})) {
 		t.Error(cmp.Diff(want, got))
 	}
 }
@@ -134,6 +135,32 @@ func TestSetPriceCentsInvalid(t *testing.T) {
 	err := b.SetPriceCents(-1)
 	if err == nil {
 		t.Fatal("Want error setting invalid price -1; got nil.")
+	}
+}
+
+func TestSetCategory(t *testing.T) {
+	t.Parallel()
+	b := bookstore.Book{
+		Title: "For the Love of Go",
+	}
+	want := "Autobiography"
+	err := b.SetCategory(want)
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := b.Category()
+	if want != got {
+		t.Errorf("Want category %q; got %q.", want, got)
+	}
+}
+
+func TestSetCategoryInvalid(t *testing.T) {
+	b := bookstore.Book{
+		Title: "For the Love of Go",
+	}
+	err := b.SetCategory("Bogus")
+	if err == nil {
+		t.Fatal("Want error for invalid category; got nil.")
 	}
 }
 
