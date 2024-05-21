85327a5c260a8f9b1bf46f1b7c828e3e627438b1
Andy <97757029+AbnormalAndy@users.noreply.github.com>
2024-05-21 18:52:03 -0500

    Practice with mytypes.

--- ForTheLoveOfGo/my-types/mytypes.go
+++ ForTheLoveOfGo/my-types/mytypes.go
@@ -1,8 +1,17 @@
 package mytypes
 
+import (
+	"strings"
+)
+
 type MyInt int
 
 func (i MyInt) Twice() MyInt {
 	return i * 2
 }
 
+type MyBuilder strings.Builder
+
+func (mb MyBuilder) Hello() string {
+	return "Hello, Gophers!"
+}
--- ForTheLoveOfGo/my-types/mytypes_test.go
+++ ForTheLoveOfGo/my-types/mytypes_test.go
@@ -2,6 +2,7 @@ package mytypes_test
 
 import (
 	"mytypes"
+	"strings"
 	"testing"
 )
 
@@ -14,3 +15,41 @@ func TestTwice(t *testing.T) {
 		t.Errorf("Twice %d: Want %d; got %d.", input, want, got)
 	}
 }
+
+func TestStringsBuilder(t *testing.T) {
+	t.Parallel()
+	var sb strings.Builder
+	sb.WriteString("Hello, ")
+	sb.WriteString("Gophers!")
+	want := "Hello, Gophers!"
+	got := sb.String()
+	if want != got {
+		t.Errorf("Want %q; got %q.", want, got)
+	}
+	wantLen := 15
+	gotLen := sb.Len()
+	if wantLen != gotLen {
+		t.Errorf("%q: Want len %d; got %d.", sb.String(), wantLen, gotLen)
+	}
+}
+
+//func TestMyBuilderLen(t *testing.T) {
+//	t.Parallel()
+//	var mb mytypes.MyBuilder
+//	want := 15
+//	got := mb.Len()
+//	if want != got {
+//		t.Errorf("Want %d; got %d.", want, got)
+//	}
+//}
+
+func TestMyBuilderHello(t *testing.T) {
+	t.Parallel()
+	var mb mytypes.MyBuilder
+	want := "Hello, Gophers!"
+	got := mb.Hello()
+	if want != got {
+		t.Errorf("Want %q; got %q.", want, got)
+	}
+}
+
