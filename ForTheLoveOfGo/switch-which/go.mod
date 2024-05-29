2b69e7ad3e26c62a0935065565c351416fd1ed2a
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-05-29 13:21:13 -0500

    Practice switch cases.

---
+++ ForTheLoveOfGo/switch-which/go.mod
@@ -0,0 +1,3 @@
+module switchwhich
+
+go 1.22.3
---
+++ ForTheLoveOfGo/switch-which/switchwhich.go
@@ -0,0 +1,11 @@
+package switchwhich
+
+switch {
+case x < 0 :
+	fmt.Println("Negative")
+case x > 0:
+	fmt.Println("Positive")
+default:
+	fmt.Println("Zero")
+}
+
