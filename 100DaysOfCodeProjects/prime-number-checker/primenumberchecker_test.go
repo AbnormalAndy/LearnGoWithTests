e1093cf253513eb38a38dd5447b105beb04680c8
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-06-07 19:35:11 -0500

    Add test.

--- 100DaysOfCodeProjects/prime-number-checker/primenumberchecker_test.go
+++ 100DaysOfCodeProjects/prime-number-checker/primenumberchecker_test.go
@@ -7,5 +7,9 @@ import (
 
 func TestPrimeNumberChecker(t *testing.T) {
 	t.Parallel()
-
+	want := "It is a prime number."
+	got := primenumberchecker.PrimeNumberChecker(73)
+	if want != got {
+		t.Errorf("Want %s; got %s.", want, got)
+	}
 }
