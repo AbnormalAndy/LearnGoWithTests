deb9d410133cc8f3acb52729b1909c585811270b
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-06-09 10:08:47 -0500

    Update prime number checker.

--- 100DaysOfCodeProjects/prime-number-checker/primenumberchecker.go
+++ 100DaysOfCodeProjects/prime-number-checker/primenumberchecker.go
@@ -1,5 +1,18 @@
 package primenumberchecker
 
+import (
+	"math"
+)
+
 func PrimeNumberChecker(number int) string {
-	return ""
+	if number < 2 {
+		return "Number must be greater than 2."
+	}
+	sq_root := int(math.Sqrt(float64(number)))
+	for i := 2; i <= sq_root; i++ {
+		if number % i == 0 {
+			return "Not a prime number."
+		}
+	}
+	return "It is a prime number."
 }
