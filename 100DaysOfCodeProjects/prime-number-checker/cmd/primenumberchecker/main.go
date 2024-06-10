4c75a4eaebad9123d909035d9dcb019c47f1fec8
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-06-10 18:32:39 -0500

    Update PrimeNumberChecker to include main.go file.

---
+++ 100DaysOfCodeProjects/prime-number-checker/cmd/primenumberchecker/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"primenumberchecker"
+)
+
+func main() {
+	fmt.Printf("Number 73: %s\n\n", primenumberchecker.PrimeNumberChecker(73))
+
+	fmt.Printf("Number 75: %s\n\n", primenumberchecker.PrimeNumberChecker(75))
+}
