2e5627746facc7f278edfe13284ac65364b792bd
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-06-12 17:17:31 -0500

    Add code to gradingprogram.go file.

--- 100DaysOfCodeProjects/grading-program/gradingprogram.go
+++ 100DaysOfCodeProjects/grading-program/gradingprogram.go
@@ -1,5 +1,14 @@
 package gradingprogram
 
 func GradeProgram(score int) string {
-	return ""
+	switch {
+	case score > 90:
+		return "Outstanding"
+	case score > 80:
+		return "Exceeds Expectations"
+	case score > 70:
+		return "Acceptable"
+	default:
+		return "Fail"
+	}
 }
