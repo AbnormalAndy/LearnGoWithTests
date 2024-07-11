984736135c7becb24d6e2efd51a592946a34ff8a
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-07-10 20:47:23 -0500

    Update Days in Month.

---
+++ 100DaysOfCodeProjects/days-in-month/cmd/daysinmonth/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"daysinmonth"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(daysinmonth.DaysInMonth(2020, 2))
+	fmt.Println(daysinmonth.DaysInMonth(2021, 2))
+	fmt.Println(daysinmonth.DaysInMonth(2022, 2))
+	fmt.Println(daysinmonth.DaysInMonth(2023, 2))
+	fmt.Println(daysinmonth.DaysInMonth(2024, 2))
+}
--- 100DaysOfCodeProjects/days-in-month/daysinmonth.go
+++ 100DaysOfCodeProjects/days-in-month/daysinmonth.go
@@ -1,24 +1,19 @@
 package daysinmonth
 
-var DaysInMonths = []int{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+//import "fmt"
+
+//var DaysInMonths = []int{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
 
 func IsLeap(year int) bool {
-	if year % 4 == 0 {
-		if year % 100 == 0 {
-			if year % 400 == 0 {
-				return true
-			} else {
-				return false
-			}
-		} else {
-			return true
-		}
+	if year % 4 == 0 && year % 100 != 0 || year % 400 == 0 {
+		return true
 	} else {
 		return false
 	}
 }
 
 func DaysInMonth(year int, month int) int {
+	var DaysInMonths = []int{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
 	if IsLeap(year) == true {
 		DaysInMonths[1] += 1
 	}
