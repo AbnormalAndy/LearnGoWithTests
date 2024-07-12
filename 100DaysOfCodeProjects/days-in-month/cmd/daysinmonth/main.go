c456b0426e996220bad1c535428710ef9c7a78f1
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-07-11 19:19:43 -0500

    Update Days in Month.

--- 100DaysOfCodeProjects/days-in-month/cmd/daysinmonth/main.go
+++ 100DaysOfCodeProjects/days-in-month/cmd/daysinmonth/main.go
@@ -6,9 +6,9 @@ import (
 )
 
 func main() {
-	fmt.Println(daysinmonth.DaysInMonth(2020, 2))
-	fmt.Println(daysinmonth.DaysInMonth(2021, 2))
-	fmt.Println(daysinmonth.DaysInMonth(2022, 2))
-	fmt.Println(daysinmonth.DaysInMonth(2023, 2))
-	fmt.Println(daysinmonth.DaysInMonth(2024, 2))
+	fmt.Printf("Number of days in February 2020: %d.\n", daysinmonth.DaysInMonth(2020, 2))
+	fmt.Printf("Number of days in February 2021: %d.\n", daysinmonth.DaysInMonth(2021, 2))
+	fmt.Printf("Number of days in February 2022: %d.\n", daysinmonth.DaysInMonth(2022, 2))
+	fmt.Printf("Number of days in February 2023: %d.\n", daysinmonth.DaysInMonth(2023, 2))
+	fmt.Printf("Number of days in February 2024: %d.\n", daysinmonth.DaysInMonth(2024, 2))
 }
