e35bfae3b4953228ecf88a4f24f0f591cc0248fc
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-07-28 15:30:57 -0500

    Update main.go file.

--- 100DaysOfCodeProjects/calculator-100days/cmd/calculator/main.go
+++ 100DaysOfCodeProjects/calculator-100days/cmd/calculator/main.go
@@ -2,7 +2,26 @@ package main
 
 import (
 	"calculator"
+	"fmt"
 )
 
 func main() {
+
+	x := 3.0
+	y := 5.0
+	input := "+"
+	total := 0.0
+
+	switch {
+	case input == "+":
+		total += calculator.Add(x, y)
+	case input == "-":
+		calculator.Subtract(x, y)
+	case input == "*":
+		calculator.Multiply(x, y)
+	case input == "/":
+		calculator.Divide(x, y)
+	}
+
+	fmt.Printf("Total: %f", total)
 }
