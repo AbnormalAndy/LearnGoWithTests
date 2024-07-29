d7cc86a98aa382284b528f2614bb50bf7c545a3f
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-07-29 12:53:07 -0500

    Update main.go file.

--- 100DaysOfCodeProjects/calculator-100days/cmd/calculator/main.go
+++ 100DaysOfCodeProjects/calculator-100days/cmd/calculator/main.go
@@ -1,27 +1,60 @@
 package main
 
 import (
+	"bufio"
 	"calculator"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 
-	x := 3.0
-	y := 5.0
-	input := "+"
-	total := 0.0
-
-	switch {
-	case input == "+":
-		total += calculator.Add(x, y)
-	case input == "-":
-		calculator.Subtract(x, y)
-	case input == "*":
-		calculator.Multiply(x, y)
-	case input == "/":
-		calculator.Divide(x, y)
+	// Ask for the first number.
+	fmt.Println("What is the first number?\n")
+	inputReaderOne := bufio.NewReader(os.Stdin)
+	firstNumberInput, err := inputReaderOne.ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+	}
+	firstNumberInput = strings.TrimSuffix(firstNumberInput, "\n")
+	firstNumber, err := strconv.ParseFloat(firstNumberInput, 64)
+	if err != nil {
+		fmt.Println(err)
 	}
 
-	fmt.Printf("Total: %f", total)
+	// Ask for the second number.
+	fmt.Println("What is the second number?\n")
+	inputReaderTwo := bufio.NewReader(os.Stdin)
+	secondNumberInput, err := inputReaderTwo.ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+	}
+	secondNumberInput = strings.TrimSuffix(secondNumberInput, "\n")
+	secondNumber, err := strconv.ParseFloat(secondNumberInput, 64)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var operator string 
+
+	fmt.Println("Choose an Operator: + - * /\n")
+	fmt.Scanln(&operator)
+	
+	sum := 0.0
+
+
+	switch {
+	case operator == "+":
+		sum = calculator.Add(firstNumber, secondNumber)
+	case operator == "-":
+		sum = calculator.Subtract(firstNumber, secondNumber)
+	case operator == "*":
+		sum = calculator.Multiply(firstNumber, secondNumber)
+	case operator == "/":
+		sum, _ = calculator.Divide(firstNumber, secondNumber)
+	}
+
+	fmt.Printf("Total: %.2f.\n", sum)
 }
