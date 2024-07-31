b0e259b2a0cf7d46f1257278dee8aa49180c9a6e
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-07-30 22:00:20 -0500

    Update main.go file.

--- 100DaysOfCodeProjects/calculator-100days/cmd/calculator/main.go
+++ 100DaysOfCodeProjects/calculator-100days/cmd/calculator/main.go
@@ -24,37 +24,59 @@ func main() {
 		fmt.Println(err)
 	}
 
-	// Ask for the second number.
-	fmt.Println("What is the second number?\n")
-	inputReaderTwo := bufio.NewReader(os.Stdin)
-	secondNumberInput, err := inputReaderTwo.ReadString('\n')
-	if err != nil {
-		fmt.Println(err)
-	}
-	secondNumberInput = strings.TrimSuffix(secondNumberInput, "\n")
-	secondNumber, err := strconv.ParseFloat(secondNumberInput, 64)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	var operator string 
-
-	fmt.Println("Choose an Operator: + - * /\n")
-	fmt.Scanln(&operator)
 	
 	sum := 0.0
 
+	quit := false
 
-	switch {
-	case operator == "+":
-		sum = calculator.Add(firstNumber, secondNumber)
-	case operator == "-":
-		sum = calculator.Subtract(firstNumber, secondNumber)
-	case operator == "*":
-		sum = calculator.Multiply(firstNumber, secondNumber)
-	case operator == "/":
-		sum, _ = calculator.Divide(firstNumber, secondNumber)
+	for quit != true {
+
+		// Ask for the second number.
+		fmt.Println("What is the second number?\n")
+		inputReaderTwo := bufio.NewReader(os.Stdin)
+		secondNumberInput, err := inputReaderTwo.ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+		}
+		secondNumberInput = strings.TrimSuffix(secondNumberInput, "\n")
+		secondNumber, err := strconv.ParseFloat(secondNumberInput, 64)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		var operator string 
+
+		fmt.Println("Choose an Operator: + - * /\n")
+		fmt.Scanln(&operator)
+
+
+		switch {
+		case operator == "+":
+			sum = calculator.Add(firstNumber, secondNumber)
+		case operator == "-":
+			sum = calculator.Subtract(firstNumber, secondNumber)
+		case operator == "*":
+			sum = calculator.Multiply(firstNumber, secondNumber)
+		case operator == "/":
+			sum, _ = calculator.Divide(firstNumber, secondNumber)
+		}
+		fmt.Printf("Sum: %.2f.\n", sum)
+
+		
+		var calculateMore string
+
+		fmt.Println("Would you like to continue? Yes or No\n")
+		fmt.Scanln(&calculateMore)
+
+		if strings.ToLower(calculateMore) == "no" || strings.ToLower(calculateMore) == "n" {
+			quit = true
+		}
+
+		if strings.ToLower(calculateMore) != "yes" || strings.ToLower(calculateMore) == "y" {
+			fmt.Println("Did not enter 'Yes' or 'No'.")
+			quit = true
+		}
+
+		firstNumber = sum
 	}
-
-	fmt.Printf("Total: %.2f.\n", sum)
 }
