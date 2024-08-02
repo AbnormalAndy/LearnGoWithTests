27752ec65242aefe1ee577a03422b1d56a4516f1
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-08-01 21:25:36 -0500

    Add AddCards function with test.

--- 100DaysOfCodeProjects/blackjack-game/blackjack.go
+++ 100DaysOfCodeProjects/blackjack-game/blackjack.go
@@ -1,3 +1,16 @@
 package blackjack
 
+func AddCards(cardOne, cardTwo int) int {
+	sum := cardOne + cardTwo
+
+	if sum > 21 && cardOne == 11 {
+		return 1 + cardTwo
+	}
+
+	if sum > 21 && cardTwo == 11 {
+		return cardOne + 1
+	}
+
+	return sum
+}
 
--- 100DaysOfCodeProjects/blackjack-game/blackjack_test.go
+++ 100DaysOfCodeProjects/blackjack-game/blackjack_test.go
@@ -5,4 +5,25 @@ import (
 	"testing"
 )
 
+func TestAddCards(t *testing.T) {
+	t.Parallel()
+	type testCase struct {
+		a, b	int
+		want	int
+	}
+	testCases := []testCase{
+		{a: 11, b: 2, want: 13},
+		{a: 5, b: 7, want: 12},
+		{a: 10, b: 11, want: 21},
+		{a: 11, b: 13, want: 14},
+		{a: 15, b: 11, want: 16},
+		{a: 11, b: 11, want: 12},
+	}
+	for _, tc := range testCases {
+		got := blackjack.AddCards(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("Want: %d; got: %d.", tc.want, got)
+		}
+	}
+}
 
