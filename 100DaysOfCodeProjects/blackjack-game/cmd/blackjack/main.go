ea952b7e47e1cc5fa2f2d2749092669fc4ed2acb
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-08-04 16:31:27 -0500

    Update main.go to play computer's blackjack hand.

--- 100DaysOfCodeProjects/blackjack-game/cmd/blackjack/main.go
+++ 100DaysOfCodeProjects/blackjack-game/cmd/blackjack/main.go
@@ -8,27 +8,35 @@ import (
 func main() {
 	deck := [13]int{11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10}
 
-	dealerCardOne := blackjack.DealCard(deck)
-	dealerCardTwo := blackjack.DealCard(deck)
-	fmt.Printf("Dealer's card one: %d; card two: %d.\n", dealerCardOne, dealerCardTwo)
+	var dealerHand []int
+	
+	dealerHand = append(dealerHand, blackjack.DealCard(deck))
+	fmt.Println(dealerHand)
 
-	dealerSum := blackjack.AddCards(dealerCardOne, dealerCardTwo)
+	dealerHand = append(dealerHand, blackjack.DealCard(deck))
+	fmt.Println(dealerHand)
+	fmt.Printf("Dealer's hand: %v.\n\n", dealerHand)
+
+	dealerSum := 0
+
+	dealerSum = blackjack.AddCards(dealerHand)
 	fmt.Printf("Dealer's sum: %d.\n\n", dealerSum)
 
-	if dealerSum < 17 {
-		dealerCardThree := blackjack.DealCard(deck)
-		fmt.Printf("Dealer's card three: %d.\n", dealerCardThree)
-		dealerSum := blackjack.AddCards(dealerSum, dealerCardThree)
+	for dealerSum < 17 {
+		dealerHand = append(dealerHand, blackjack.DealCard(deck))
+		fmt.Printf("Dealer's hand: %v.\n\n", dealerHand)
+
+		dealerSum = blackjack.AddCards(dealerHand)	
 		fmt.Printf("Dealer's sum: %d.\n\n", dealerSum)
-		// Drew an 11.
-		// If not greater than player, convert to 1.
-		// If greater than player with 11, wins.
 	}
 
+	// TO-DO: Code the player hand and input.
+	/*
 	playerCardOne := blackjack.DealCard(deck)
 	playerCardTwo := blackjack.DealCard(deck)
 	fmt.Printf("Player's card one: %d; card two: %d.\n", playerCardOne, playerCardTwo)
 
 	playerSum := blackjack.AddCards(playerCardOne, playerCardTwo)
 	fmt.Printf("Player's sum: %d.\n\n", playerSum)
+	*/
 }
