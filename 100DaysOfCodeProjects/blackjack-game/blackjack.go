1463ed13743a03cb474cd0c448fb41288bd76778
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-08-03 20:56:44 -0500

    Add conditional expression to AddCards function.

--- 100DaysOfCodeProjects/blackjack-game/blackjack.go
+++ 100DaysOfCodeProjects/blackjack-game/blackjack.go
@@ -7,6 +7,8 @@ import (
 // Adds cards in slice and, if the sum is more than 21, will try convert an Ace to a 1.
 // TO-DO: Figure out a way to evaluate greater than 21 hands.
 // TO-DO: Convert 11 to 1 if hand greater than 21.
+// TO-DO: Figure out a way to only conver 11 to 1 for only one of the 11's.
+// Last test is failing.
 func AddCards(hand []int) int {
 	sum := 0
 	for i := range hand {
@@ -14,18 +16,16 @@ func AddCards(hand []int) int {
 	}
 
 	if sum > 21 {
-		return 0
-	}
+		sum = 0
+		for i := range hand {
+			if hand[i] == 11 {
+				hand[i] = 1
+			}
+			sum += hand[i]
+		}
 
-	/*
-	if sum > 21 && cardOne == 11 {
-		return 1 + cardTwo
+		return sum
 	}
-
-	if sum > 21 && cardTwo == 11 {
-		return cardOne + 1
-	}
-	*/
 
 	return sum
 }
