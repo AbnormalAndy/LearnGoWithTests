f276d96de29f71d4744fe936eb92a1d4f16f3a16
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-08-15 17:00:00 -0500

    Add higher, lower, and equal tests and functions.

--- 100DaysOfCodeProjects/number-guessing-game/numberguessinggame.go
+++ 100DaysOfCodeProjects/number-guessing-game/numberguessinggame.go
@@ -1,3 +1,22 @@
 package numberguessinggame
 
+func EvaluateIfHigher(randomNumber, playerGuess int) bool {
+	if randomNumber < playerGuess {
+		return true
+	}
+	return false
+}
 
+func EvaluateIfLower(randomNumber, playerGuess int) bool {
+	if randomNumber > playerGuess {
+		return true
+	}
+	return false
+}
+
+func EvaluateIfEqual(randomNumber, playerGuess int) bool {
+	if randomNumber == playerGuess {
+		return true
+	}
+	return false
+}
--- 100DaysOfCodeProjects/number-guessing-game/numberguessinggame_test.go
+++ 100DaysOfCodeProjects/number-guessing-game/numberguessinggame_test.go
@@ -5,7 +5,59 @@ import (
 	"testing"
 )
 
-func TestEvaluateHigherOrLower(t *testing.T) {
+func TestEvaluateIfHigher(t *testing.T) {
 	t.Parallel()
+	type testCase struct {
+		a, b	int
+		want	bool
+	}
+	testCases := []testCase{
+		{a: 13, b: 11, want: false},
+		{a: 13, b: 15, want: true},
+		{a: 13, b: 13, want: false},
+	}
+	for _, tc := range testCases {
+		got := numberguessinggame.EvaluateIfHigher(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("Want %v; got %v.", tc.want, got)
+		}
+	}
+}
 
+func TestEvaluateIfLower(t *testing.T) {
+	t.Parallel()
+	type testCase struct {
+		a, b	int
+		want	bool
+	}
+	testCases := []testCase{
+		{a: 13, b: 11, want: true},
+		{a: 13, b: 13, want: false},
+		{a: 13, b: 15, want: false},
+	}
+	for _, tc := range testCases {
+		got := numberguessinggame.EvaluateIfLower(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("Want %v; got %v.", tc.want, got)
+		}
+	}
+}
+
+func TestEvaluateIfEqual(t *testing.T) {
+	t.Parallel()
+	type testCase struct {
+		a, b	int
+		want	bool
+	}
+	testCases := []testCase{
+		{a: 13, b: 11, want: false},
+		{a: 13, b: 13, want: true},
+		{a: 13, b: 15, want: false},
+	}
+	for _, tc := range testCases {
+		got := numberguessinggame.EvaluateIfEqual(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("Want %v; got %v.", tc.want, got)
+		}
+	}
 }
