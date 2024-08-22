b18d116f117c1fbab2d32923b3818708a16239b8
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-08-22 18:32:22 -0500

    Start main.go file code.

--- 100DaysOfCodeProjects/number-guessing-game/cmd/number-guess/main.go
+++ 100DaysOfCodeProjects/number-guessing-game/cmd/number-guess/main.go
@@ -7,6 +7,9 @@ import (
 
 func main() {
 	fmt.Println("Welcome to the Number Guessing Game!\n")
+	fmt.Println("I am thinking of a number between 1 and 100.\n")
+	fmt.Println("Choose a difficulty. Type 'easy' or 'hard':\n")
+
 
 }
 
