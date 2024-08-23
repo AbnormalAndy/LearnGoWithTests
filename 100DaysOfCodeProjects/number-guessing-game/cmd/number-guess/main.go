10a4f3998868f8c540ccea18d5be10af30492d49
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-08-23 12:16:21 -0500

    Update code in main.go file.

--- 100DaysOfCodeProjects/number-guessing-game/cmd/number-guess/main.go
+++ 100DaysOfCodeProjects/number-guessing-game/cmd/number-guess/main.go
@@ -1,15 +1,38 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"numberguessinggame"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Welcome to the Number Guessing Game!\n")
+	var difficulty string
+
+	fmt.Println("\nWelcome to the Number Guessing Game!\n")
 	fmt.Println("I am thinking of a number between 1 and 100.\n")
 	fmt.Println("Choose a difficulty. Type 'easy' or 'hard':\n")
+	
+	inputDifficulty := bufio.NewReader(os.Stdin)
+	difficulty, _ = inputDifficulty.ReadString('\n')
+	difficulty = strings.TrimSuffix(difficulty, "\n")
+	difficulty = strings.ToLower(difficulty)
+	
+	// May have to make this a switch.
+	if difficulty == "easy" || difficulty == "e" {
+		fmt.Println("\nYou have 10 attempts to guess the number.\n")
+	}
+	
+	if difficulty == "hard" || difficulty == "h" {
+		fmt.Println("\nYou have 5 attempts to guess the number.\n")
+	}
 
+	fmt.Println(difficulty)
 
+	randomNumber := 10
+	playerNumber := 5
+	fmt.Println(numberguessinggame.EvaluateIfLower(randomNumber, playerNumber))
 }
 
