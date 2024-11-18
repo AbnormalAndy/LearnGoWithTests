66c11e7c3db708dbe12d6eb28c5551cd2cc3a385
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-11-17 18:05:31 -0600

    Start word-guessing-game for GoLang.

---
+++ 100DaysOfCodeProjects/day-7/word-guessing-game/wordguessinggame.go
@@ -0,0 +1,20 @@
+package wordguessinggame
+
+
+var words = []string{
+	"apple",
+	"banana",
+	"broccoli",
+	"cherry",
+	"fig",
+	"garlic",
+	"kiwi",
+	"kale",
+	"lemon",
+	"leek",
+	"mango",
+	"yams",
+}
+
+
+
---
+++ 100DaysOfCodeProjects/day-7/word-guessing-game/wordguessinggame_test.go
@@ -0,0 +1,10 @@
+package wordguessinggame_test
+
+import (
+	"testing"
+	"wordguessinggame"
+)
+
+func TestMakePlayerWord(t *testing.T) {
+	t.Parallel()
+}
